59f57b2a823737722e7746f0a63fc1d05bf2c297
atharva0608 <atharvapudale0608@gmail.com>
2025-06-02 00:16:42 +0530

    environments updated

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   apiBaseUrl: 'http://spring-backend:8080/api/v1'
 };
 
 /*
